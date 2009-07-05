fcdb57c583a7c1e66faa115412ff07ac97f69094
dfstrauss@gmail.com <dfstrauss@gmail.com@86957ede-5ac9-11de-b31f-81d54249e7d9>
2009-07-05 18:04:27 +0000

    Better implementation of reading mock scenarios file

--- textmagic/test/mock_client.py
+++ textmagic/test/mock_client.py
@@ -24,17 +24,16 @@ class MockTextMagicClient(_TextMagicClientBase):
     @classmethod
     def _load_canned_responses(self):
         self.canned_responses = list()
-        lines = open('mock_client_scenarios.txt').readlines()
-        idx = 0
-        while idx < len(lines):
-            line = lines[idx]
-            if line.startswith('Parameters:'):
-                params = line[11:].strip()
-                idx += 1
-                line = lines[idx]
+        params_start = 'Parameters:'
+        response_start = 'Response:'
+        scenarios_file = open('mock_client_scenarios.txt')
+        for line in scenarios_file:
+            if line.startswith(params_start):
+                params = line[len(params_start):].strip()
                 # A "Response:" line must follow a "Parameters:" line
-                assert line.startswith('Response:')
-                response = line[9:].strip()
+                line = scenarios_file.next()
+                assert line.startswith(response_start)
+                response = line[len(response_start):].strip()
                 parameters = {}
                 for param in params.split('&'):
                     name, value = param.split('=')
@@ -42,7 +41,7 @@ class MockTextMagicClient(_TextMagicClientBase):
                 del parameters['username']
                 del parameters['password']
                 self.canned_responses.append([parameters, response])
-            idx += 1
+        scenarios_file.close()
 
     def __init__(self, username, password):
         super(MockTextMagicClient, self).__init__(username, password)
